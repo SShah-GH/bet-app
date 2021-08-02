cf2523f4835a6f5c23210e1f441ebadeb9ddebb3
melina-diaz <82481505+melina-diaz@users.noreply.github.com>
2021-08-01 18:25:59 -0700

    Made Bet Details Won

--- screens/index.js
+++ screens/index.js
@@ -5,6 +5,7 @@ import DashBoard from "./DashBoard";
 import Recovery from "./Recovery";
 import CashConfirm from "./CashConfirm";
 import DareConfirm from "./DareConfirm";
+import BetDetailsWon from "./BetDetailsWon";
 
 
 export{
@@ -14,5 +15,6 @@ export{
     DashBoard,
     Recovery,
     CashConfirm,
-    DareConfirm
+    DareConfirm,
+    BetDetailsWon
 };
